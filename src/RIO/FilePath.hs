4962ef2e89c3de3c3ecb13bf4f04d72cc6d4e974
Iris Ward <aditu.venyhandottir@gmail.com>
2018-03-08 18:45:31 +0000

    Lift IO for FilePath, Time

--- src/RIO/FilePath.hs
+++ src/RIO/FilePath.hs
@@ -1,5 +1,12 @@
 module RIO.FilePath
   ( module System.FilePath
+  , getSearchPath
   ) where
 
-import System.FilePath
+import Control.Monad.IO.Class
+import System.FilePath hiding(getSearchPath)
+import qualified System.FilePath
+
+-- | Lifted version of 'System.FilePath.getSearchPath'
+getSearchPath :: MonadIO m => m [FilePath]
+getSearchPath = liftIO System.FilePath.getSearchPath
--- src/RIO/Time.hs
+++ src/RIO/Time.hs
@@ -1,5 +1,28 @@
 module RIO.Time
   ( module Data.Time
+  , getCurrentTime
+  , getTimeZone
+  , getCurrentTimeZone
+  , getZonedTime
+  , utcToLocalZonedTime
   ) where
 
-import Data.Time
+import Control.Monad.IO.Class
+import Data.Time hiding( getCurrentTime, getTimeZone, getCurrentTimeZone
+                       , getZonedTime, utcToLocalZonedTime)
+import qualified Data.Time
+
+getCurrentTime :: MonadIO m => m UTCTime
+getCurrentTime = liftIO Data.Time.getCurrentTime
+
+getTimeZone :: MonadIO m => UTCTime -> m TimeZone
+getTimeZone = liftIO . Data.Time.getTimeZone
+
+getCurrentTimeZone :: MonadIO m => m TimeZone
+getCurrentTimeZone = liftIO Data.Time.getCurrentTimeZone
+
+getZonedTime :: MonadIO m => m ZonedTime
+getZonedTime = liftIO Data.Time.getZonedTime
+
+utcToLocalZonedTime :: MonadIO m => UTCTime -> m ZonedTime
+utcToLocalZonedTime = liftIO . Data.Time.utcToLocalZonedTime
