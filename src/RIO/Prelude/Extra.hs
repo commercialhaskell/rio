3a4ecbfb421db7899fc94d08565227b38cd41012
Michael Snoyman <michael@snoyman.com>
2018-03-13 11:50:57 +0200

    Add whenM and unlessM

--- src/RIO/Prelude/Extra.hs
+++ src/RIO/Prelude/Extra.hs
@@ -7,16 +7,20 @@ module RIO.Prelude.Extra
   , forMaybeA
   , forMaybeM
   , nubOrd
+  , whenM
+  , unlessM
   ) where
 
 import qualified Data.Set as Set
 import Data.Monoid (First (..))
 import RIO.Prelude.Reexports
 
+-- | Apply a function to a 'Left' constructor
 mapLeft :: (a1 -> a2) -> Either a1 b -> Either a2 b
 mapLeft f (Left a1) = Left (f a1)
 mapLeft _ (Right b) = Right b
 
+-- | Get a 'First' value with a default fallback
 fromFirst :: a -> First a -> a
 fromFirst x = fromMaybe x . getFirst
 
@@ -36,6 +40,7 @@ mapMaybeM f = liftM catMaybes . mapM f
 forMaybeM :: Monad m => [a] -> (a -> m (Maybe b)) -> m [b]
 forMaybeM = flip mapMaybeM
 
+-- | Strip out duplicates
 nubOrd :: Ord a => [a] -> [a]
 nubOrd =
   loop mempty
@@ -44,3 +49,15 @@ nubOrd =
     loop !s (a:as)
       | a `Set.member` s = loop s as
       | otherwise = a : loop (Set.insert a s) as
+
+-- | Run the second value if the first value returns 'True'
+whenM :: Monad m => m Bool -> m () -> m ()
+whenM boolM action = do
+  x <- boolM
+  if x then action else return ()
+
+-- | Run the second value if the first value returns 'False'
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM boolM action = do
+  x <- boolM
+  if x then return () else action
--- test/RIO/PreludeSpec.hs
+++ test/RIO/PreludeSpec.hs
@@ -7,13 +7,32 @@ import RIO
 
 spec :: Spec
 spec = do
-  it "sanity" $ do
-    ref <- newURef (0 :: Int)
-    x <- readURef ref
-    x `shouldBe` 0
-    writeURef ref 1
-    y <- readURef ref
-    y `shouldBe` 1
-    modifyURef ref (+ 1)
-    z <- readURef ref
-    z `shouldBe` 2
+  describe "URef" $ do
+    it "sanity" $ do
+      ref <- newURef (0 :: Int)
+      x <- readURef ref
+      x `shouldBe` 0
+      writeURef ref 1
+      y <- readURef ref
+      y `shouldBe` 1
+      modifyURef ref (+ 1)
+      z <- readURef ref
+      z `shouldBe` 2
+  describe "whenM" $ do
+    it "returns True" $ do
+      ref <- newIORef False
+      whenM (return True) (writeIORef ref True)
+      readIORef ref `shouldReturn` True
+    it "returns False" $ do
+      ref <- newIORef False
+      whenM (return False) (writeIORef ref True)
+      readIORef ref `shouldReturn` False
+  describe "unlessM" $ do
+    it "returns True" $ do
+      ref <- newIORef False
+      unlessM (return True) (writeIORef ref True)
+      readIORef ref `shouldReturn` False
+    it "returns False" $ do
+      ref <- newIORef False
+      unlessM (return False) (writeIORef ref True)
+      readIORef ref `shouldReturn` True
