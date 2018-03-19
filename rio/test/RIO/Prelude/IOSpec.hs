ec566c731dd096f76db32e26c94cd43df4235a20
Michael Snoyman <michael@snoyman.com>
2018-03-19 18:33:31 +0200

    Use textual I/O to handle line endings
    This allows the handle API to automatically deal with LF vs CRLF line
    endings correctly. Pointed out to me _somewhere_ by @sol, I don't
    remember where I saw it.

---
+++ rio/test/RIO/Prelude/IOSpec.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ViewPatterns #-}
+module RIO.Prelude.IOSpec (spec) where
+
+import RIO
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import qualified RIO.ByteString as B
+import qualified RIO.Text as T
+
+spec :: Spec
+spec = do
+  prop "binary file read/write" $ \(B.pack -> bs1) ->
+    withSystemTempFile "binary-read-write" $ \fp h -> do
+      hClose h
+      writeFileBinary fp bs1
+      bs2 <- readFileBinary fp
+      bs2 `shouldBe` bs1
+  prop "text file read/write" $ \(T.pack -> text1) ->
+    withSystemTempFile "binary-read-write" $ \fp h -> do
+      hClose h
+      writeFileUtf8 fp text1
+      text2 <- readFileUtf8 fp
+      text2 `shouldBe` text1
