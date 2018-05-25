29acba97c2acc7957cebce305fa11b565108c4bb
Roman Gonzalez <romanandreg@gmail.com>
2018-05-25 04:25:16 +0000

    Allow reload for logMinLevel and logVerboseFormat settings

--- rio/test/RIO/LoggerSpec.hs
+++ rio/test/RIO/LoggerSpec.hs
@@ -24,3 +24,25 @@ spec = do
       logStickyDone "XYZ"
     builder <- readIORef ref
     toLazyByteString builder `shouldBe` "ABC\b\b\b   \b\b\bshould appear\nABC\b\b\b   \b\b\bXYZ\n"
+  it "setLogMinLevelIO" $ do
+    (ref, options) <- logOptionsMemory
+    logLevelRef <- newIORef LevelDebug
+    withLogFunc (options & setLogMinLevelIO (readIORef logLevelRef))
+      $ \lf -> runRIO lf $ do
+        logDebug "should appear"
+        -- reset log min level to info
+        atomicModifyIORef' logLevelRef (\_ -> (LevelInfo, ()))
+        logDebug "should not appear"
+    builder <- readIORef ref
+    toLazyByteString builder `shouldBe` "should appear\n"
+  it "setLogVerboseFormatIO" $ do
+    (ref, options) <- logOptionsMemory
+    logVerboseFormatRef <- newIORef True
+    withLogFunc (options & setLogVerboseFormatIO (readIORef logVerboseFormatRef))
+      $ \lf -> runRIO lf $ do
+        logInfo "verbose log"
+        -- reset verbose format
+        atomicModifyIORef' logVerboseFormatRef (\_ -> (False, ()))
+        logInfo "no verbose log"
+    builder <- readIORef ref
+    toLazyByteString builder `shouldBe` "[info] verbose log\nno verbose log\n"
