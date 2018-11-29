b38147d1606700dd33988147763935d7bb6c1e7e
Michael Snoyman <michael@snoyman.com>
2018-11-29 09:46:19 -0800

    Fix function name on documentation #review
    Co-Authored-By: roman <roman@users.noreply.github.com>

--- rio/src/RIO/File.hs
+++ rio/src/RIO/File.hs
@@ -354,7 +354,7 @@ writeBinaryFileDurableAtomic fp bytes =
 --
 -- === Cross-Platform support
 --
--- This function behaves the same as 'System.IO.withFile' on Windows platforms.
+-- This function behaves the same as 'System.IO.withBinaryFile' on Windows platforms.
 --
 -- @since 0.1.6
 withBinaryFileDurable ::
