a0634d104d43dc2cf198f0a40d79c1d5150c94c0
Michael Snoyman <michael@snoyman.com>
2019-02-28 12:34:16 +0200

    Clean up warnings

--- rio/src/RIO/State.hs
+++ rio/src/RIO/State.hs
@@ -7,14 +7,13 @@ module RIO.State
   , Control.Monad.State.gets
   , Control.Monad.State.modify
   , Control.Monad.State.modify'
-  , Control.Monad.State.State (..)
+  , Control.Monad.State.State
   , Control.Monad.State.runState
   , Control.Monad.State.evalState
   , Control.Monad.State.execState
   , Control.Monad.State.mapState
   , Control.Monad.State.withState
   , Control.Monad.State.StateT (..)
-  , Control.Monad.State.runStateT
   , Control.Monad.State.evalStateT
   , Control.Monad.State.execStateT
   , Control.Monad.State.mapStateT
