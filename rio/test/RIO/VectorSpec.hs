0e599574237c271498e7cb3081acdcc0ca603889
Björn Buckwalter <bjorn@buckwalter.se>
2019-09-23 15:51:16 +0200

    Property testing.
    Special casing is required due to GHC issue
    <https://gitlab.haskell.org/ghc/ghc/issues/17233>.

--- rio/test/RIO/VectorSpec.hs
+++ rio/test/RIO/VectorSpec.hs
@@ -2,27 +2,33 @@
 module RIO.VectorSpec where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import qualified Test.QuickCheck as QC
 import RIO
-import qualified RIO.Vector.Boxed as VB
+import qualified RIO.Vector as V
+import qualified RIO.Vector.Partial as V'
 
 spec :: Spec
 spec =
   describe "sliceMaybe" $ do
-    it "fails on negative index" $
-      VB.sliceMaybe (-1) 0 v `shouldBe` Nothing
-    it "fails on negative slice length" $
-      VB.sliceMaybe 0 (-1) v `shouldBe` Nothing
-    it "fails when index too large" $
-      VB.sliceMaybe (VB.length v + 1) 0 v `shouldBe` Nothing
-    it "fails when slice too large" $
-      VB.sliceMaybe 0 (VB.length v + 1) v `shouldBe` Nothing
-    it "works for all of vector" $
-      VB.sliceMaybe 0 (VB.length v) v `shouldBe` Just v
-    it "works for zero slice length" $
-      VB.sliceMaybe 0 0 v `shouldBe` Just mempty
-    it "works for zero vector" $
-      VB.sliceMaybe 0 0 (mempty :: VB.Vector ()) `shouldBe` Just mempty
-    it "works for end of vector" $
-      VB.sliceMaybe (VB.length v) 0 v `shouldBe` Just mempty
+    prop "is consistent with `slice` (pathological cases)" $
+      \(QC.Large i) (QC.Large n) v
+        -> sliceTest i n (V.fromList v)
+    -- The next property is a subset of the previous one but with
+    -- significantly greater likelyhood of having "realistic"
+    -- arguments to `slice`.
+    prop "is consistent with `slice` (more realistic cases)" $
+      \(QC.NonNegative i) (QC.NonNegative n) (QC.NonEmpty v)
+        -> sliceTest i n (V.fromList v)
   where
-    v = VB.fromList "foobar"
+    sliceTest :: Int -> Int -> Vector Char -> QC.Property
+    sliceTest i n v = QC.withMaxSuccess 1000 $ case V.sliceMaybe i n v of
+      Just v' -> V'.slice i n v `shouldBe` v'
+      Nothing -> do
+        -- Special case handling for cases when `i+n` overflows. This is
+        -- required due to <https://gitlab.haskell.org/ghc/ghc/issues/17233>.
+        -- Once that GHC issue is closed the `when` clause can be removed.
+        -- (Negative overflow is not an issue as an exception is thrown for
+        -- negative arguments.)
+        when (i > 0 && n > 0 && n + i < 0) QC.discard  -- `i+n` overflows
+        evaluate (V'.slice i n v) `shouldThrow` anyException
